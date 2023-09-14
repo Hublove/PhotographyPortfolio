3c0d622a9e612b6e5157bdb0bafd5e5dd1e54cb7
unknown <runemaster52@gmail.com>
2023-09-13 23:11:47 -0400

    updated old jpgs from main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <div class="flex flex-col items-center">
-    <Card text="Portraits" side="left" internalLink="/portraits" img ="https://cdn.discordapp.com/attachments/1060077869969178674/1144800654708191312/King_Jasoin_Landscape_Grain.jpg"/>
-    <Card text="Street" side="right" internalLink="/street" img ="https://cdn.discordapp.com/attachments/973435551514509422/1103542450561237102/Red-Flare-Younge.jpg"/>
-    <Card text="Travel" side="left" internalLink="/travel" img="https://cdn.discordapp.com/attachments/973435551514509422/1103542596598505514/Wheat-Field.jpg"/>    
-    <Card text="Events" side="right" internalLink="/events" img="https://cdn.discordapp.com/attachments/1060077869969178674/1144802202406367292/3---Lit-Head.jpg"/>
+    <Card text="Portraits" side="left" internalLink="/portraits" img ="/covers/King_Jasoin_Landscape_Grain.webp"/>
+    <Card text="Street" side="right" internalLink="/street" img ="/covers/Red-Flare-Younge.webp"/>
+    <Card text="Travel" side="left" internalLink="/travel" img="/covers/Wheat-Field.webp"/>    
+    <Card text="Events" side="right" internalLink="/events" img="/covers/3---Lit-Head.webp"/>
 </div>
