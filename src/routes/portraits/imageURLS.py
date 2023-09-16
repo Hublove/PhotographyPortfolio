b8127e88a8a08b8c21d2f80777014ed8b24b3c90
unknown <runemaster52@gmail.com>
2023-09-15 22:47:32 -0400

    Removed pictures from site

--- src/routes/portraits/imageURLS.py
+++ src/routes/portraits/imageURLS.py
@@ -5,40 +5,41 @@ from PIL import Image
 import os
 # assign directory
 # Portraits Events Travel Street
-category = "Street" #CHANGE THIS
-directory = 'static/' + category
-images = []
- 
-# iterate over files in
-# that directory
-for filename in os.listdir(directory):
-    f = os.path.join(directory, filename)
-    # checking if it is a file
-    if os.path.isfile(f):
-        if filename != "favicon.png" and filename != category + ".json":
-            # get image
-            print(f)
+categories = ["Portraits", "Events", "Travel", "Street"]
+for category in categories:
+    directory = 'static/' + category
+    images = []
+    
+    # iterate over files in
+    # that directory
+    for filename in os.listdir(directory):
+        f = os.path.join(directory, filename)
+        # checking if it is a file
+        if os.path.isfile(f):
+            if filename != "favicon.png" and filename != category + ".json":
+                # get image
+                print(f)
 
-            img = Image.open(f)
+                img = Image.open(f)
 
-            # get width and height
-            width = img.width
-            height = img.height
+                # get width and height
+                width = img.width
+                height = img.height
 
-            # display width and height
-            images.append({
-                "img": "/"+category+"/"+filename,
-                "thumb": "/"+category+"/thumbnails/"+filename,
-                "alt": '',
-                "height": height,
-                "width": width,
-                "element": None
-            })
-    print(images)
+                # display width and height
+                images.append({
+                    "img": "/"+category+"/"+filename,
+                    "thumb": "/"+category+"/thumbnails/"+filename,
+                    "alt": '',
+                    "height": height,
+                    "width": width,
+                    "element": None
+                })
+        print(images)
+                
             
-        
-with open("src/" + category + ".json", "w") as outfile:
-    json.dump(images, outfile)
-    print("DONE")
+    with open("src/" + category + ".json", "w") as outfile:
+        json.dump(images, outfile)
+        print("DONE")
 
 
